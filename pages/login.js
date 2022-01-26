4e199fc7f229896bdfdb3deec89344e2ee8e9e61
Andy Chang <andy.chang@mycommunitydirectory.com.au>
2022-01-26 13:11:19 +1000

    login to main page

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+import { getProviders, signIn } from "next-auth/react";
 
-function Login() {
-  return <div>
-      This is auth
-  </div>;
+function Login({ providers }) {
+  return (
+    <div className="flex flex-col items-center bg-black min-h-screen justify-center">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+      {Object.values(providers).map((provider) => (
+        <div>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Login;
+
+//server side render
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
