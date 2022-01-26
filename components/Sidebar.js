39442770ebb6b35058083d58698aabf4a3f0d8c3
Andy Chang <andy.chang@mycommunitydirectory.com.au>
2022-01-26 13:42:44 +1000

    applying signin and out

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,39 +5,46 @@ import {
   LibraryIcon,
   PlusCircleIcon,
   HeartIcon,
-  RssIcon
+  RssIcon,
 } from "@heroicons/react/outline";
-
+import { signOut, useSession } from "next-auth/react";
 function Sidebar() {
+  const { data: session, state } = useSession();
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log Out</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="h-5 w-5"/>
+          <HomeIcon className="h-5 w-5" />
           <p>Home</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <SearchIcon className="h-5 w-5"/>
+          <SearchIcon className="h-5 w-5" />
           <p>Search</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="h-5 w-5"/>
+          <LibraryIcon className="h-5 w-5" />
           <p>Your Library</p>
         </button>
-        <hr className="border-r-[0.1px] border-gray-900" ></hr>
+        <hr className="border-r-[0.1px] border-gray-900"></hr>
         <button className="flex items-center space-x-2 hover:text-white">
-          <PlusCircleIcon className="h-5 w-5"/>
+          <PlusCircleIcon className="h-5 w-5" />
           <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5"/>
+          <HeartIcon className="h-5 w-5" />
           <p>Liked Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5"/>
+          <RssIcon className="h-5 w-5" />
           <p>Your Exposids</p>
         </button>
-        <hr className="border-r-[0.1px] border-gray-900" ></hr>
+        <hr className="border-r-[0.1px] border-gray-900"></hr>
 
         {/* {playlist} */}
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
