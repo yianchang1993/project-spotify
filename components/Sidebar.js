bf69a512782a2bea80098b3fec23e7280bc42559
Andy Chang <andy.chang@mycommunitydirectory.com.au>
2022-01-26 18:36:56 +1000

    setup center

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,7 +11,7 @@ import { signOut, useSession } from "next-auth/react";
 function Sidebar() {
   const { data: session, state } = useSession();
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide" >
       <div className="space-y-4">
         <button
           className="flex items-center space-x-2 hover:text-white"
