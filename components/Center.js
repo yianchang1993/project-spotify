b383864f5f5b2f933561bc8673649cbf16cd6e85
Andy Chang <andy.chang@mycommunitydirectory.com.au>
2022-01-27 20:54:57 +1000

    songs list setup

--- components/Center.js
+++ components/Center.js
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { useSession,signOut } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import { shuffle } from "lodash";
 import { useRecoilValue, useRecoilState } from "recoil";
@@ -40,7 +40,8 @@ function Center() {
   return (
     <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+        <div className="flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2"
+        onClick={signOut}>
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
--- components/Song.js
+++ components/Song.js
@@ -1,21 +1,38 @@
 import React from "react";
+import { millisToMinutesAndSeconds } from "../lib/time";
+import {currentTrackIdState,isPlayingState} from "../atoms/songAtom"
+import useSporify from "../hooks/useSporify";
+import {useRecoilState} from 'recoil'
+
 
 function Song({ order, track }) {
+  const spotifyApi = useSporify();
+  const [currentTrackId, SetCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
+  const playSong = () =>{
+    SetCurrentTrackId(track.track.id)
+    setIsPlaying(true)
+    spotifyApi.play({
+      uris:[track.track.uri],
+    })
+  }
+
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 cursor:pointer rounded-lg"
+    onClick={playSong}>
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img className="h-10 w-10" src={track.track.album.images[0].url}></img>
         <div>
-          <p>{track.track.name}</p>
+          <p className="w-36 lg:w-64 truncate text-white">{track.track.name}</p>
           <p>{track.track.artists[0].name}</p>
         </div>
-        
       </div>
       <div className="flex items-center justify-between ml-auto md:ml-0">
-          <p className="hidden md:inline">{track.track.album.name}</p>
-          <p>duration</p>
-        </div>
+        <p className="w-40 hidden md:inline">{track.track.album.name}</p>
+        <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
+      </div>
     </div>
   );
 }
